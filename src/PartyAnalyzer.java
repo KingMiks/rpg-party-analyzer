561961dcf11bd35947a5af220acb427a79a34ac5
KingMiks <Miko.gaddi27@gmail.com>
2026-08-11 13:04:12 -0400

    Add role synergy rating analysis

--- src/PartyAnalyzer.java
+++ src/PartyAnalyzer.java
@@ -8,7 +8,7 @@ public class PartyAnalyzer {
         Map<Role, Integer> countRoles = new HashMap<>();
 
         if (members == null){
-                return countRoles;
+            return countRoles;
         }
         
         for (int i = 0; i < members.size(); i++){
@@ -26,29 +26,108 @@ public class PartyAnalyzer {
         return countRoles;
     }
 
+    public boolean hasDamage(Map<Role, Integer> countRoles){
+        if (countRoles == null){
+            return false;
+        }
+        return (countRoles.containsKey(Role.MELEE_DAMAGE) || countRoles.containsKey(Role.RANGED_DAMAGE));
+    }
+    public boolean hasComplementaryRole(Map<Role, Integer> countRoles){
+        if (countRoles == null){
+            return false;
+        }
+        return (countRoles.containsKey(Role.CONTROL)) || (countRoles.containsKey(Role.HEALER)) || 
+        (countRoles.containsKey(Role.TANK)) ||(countRoles.containsKey(Role.SUPPORT));
+    }
+    public boolean hasSynergy(Map<Role, Integer> countRoles){
+        if (countRoles == null){
+            return false;
+        }
+        return hasDamage(countRoles) && hasComplementaryRole(countRoles);
+    }
+
+    public int countDamageRoles(Map<Role, Integer> countRoles){
+        if (countRoles == null){
+            return 0;
+        }
+        int meleeRole = countRoles.getOrDefault(Role.MELEE_DAMAGE, 0);
+        int rangedRole = countRoles.getOrDefault(Role.RANGED_DAMAGE, 0);
+        return rangedRole + meleeRole;
+    }
+    public int countComplementaryRoles(Map<Role,Integer> countRoles){
+        if (countRoles == null){
+            return 0;
+        }
+        int tankRole = countRoles.getOrDefault(Role.TANK, 0);
+        int supportRole = countRoles.getOrDefault(Role.SUPPORT, 0);
+        int controlRole = countRoles.getOrDefault(Role.CONTROL, 0);
+        int healerRole = countRoles.getOrDefault(Role.HEALER, 0);
+        return tankRole + supportRole + controlRole + healerRole;
+    }
+    public int calculateRoleRating(Map<Role, Integer> countRoles){
+        if (countRoles == null){
+            return 0;
+        }
+        int damageRoles = countDamageRoles(countRoles);
+        int complementaryRoles = countComplementaryRoles(countRoles);
+        int partySize = damageRoles + complementaryRoles;
+        if (partySize < 2){
+            return 0;
+        }
+        int actualDifference = Math.abs(damageRoles - complementaryRoles);
+        int idealDifference = partySize % 2;
+        int distanceFromIdeal = actualDifference - idealDifference;
+        switch (distanceFromIdeal) {
+            case 0:
+                return 5;
+            case 2:
+                return 4;
+            case 4:
+                return 3;
+            case 6:
+                return 2;
+            case 8:
+                return 1;
+            default:
+                return 0;
+        }
+    }
     public static void main(String[] args){
         Party party = new Party();
         PartyAnalyzer partyAnalyzer = new PartyAnalyzer();
 
-        GameCharacter arthur = new GameCharacter("Arthur", CharacterClass.WARRIOR, Role.TANK, AbilityType.SHIELD, 100, 200, 300);
+        GameCharacter arthur = new GameCharacter("Arthur", CharacterClass.WARRIOR, Role.MELEE_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
 
-        GameCharacter lancelot = new GameCharacter("Lancelot", CharacterClass.MAGE, Role.CONTROL, AbilityType.SHIELD, 100, 200, 300);
+        GameCharacter lancelot = new GameCharacter("Lancelot", CharacterClass.MAGE, Role.RANGED_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
 
-        GameCharacter theSeparateArthur = new GameCharacter("Arthur", CharacterClass.RANGER, Role.SUPPORT, AbilityType.SHIELD, 100, 200, 300);
+        GameCharacter theSeparateArthur = new GameCharacter("Arthur", CharacterClass.RANGER, Role.MELEE_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
+
+        GameCharacter theSeparateLancelot = new GameCharacter("Arthur", CharacterClass.RANGER, Role.RANGED_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
+
+        GameCharacter moon = new GameCharacter("Arthur", CharacterClass.RANGER, Role.RANGED_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
+
+        GameCharacter sun = new GameCharacter("Arthur", CharacterClass.RANGER, Role.MELEE_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
+
+        GameCharacter star = new GameCharacter("Arthur", CharacterClass.RANGER, Role.MELEE_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
+
+        GameCharacter astroid = new GameCharacter("Arthur", CharacterClass.RANGER, Role.MELEE_DAMAGE, AbilityType.SHIELD, 100, 200, 300);
         
         System.out.println(party.addMember(arthur));
-        System.out.println(partyAnalyzer.countRoles(party.getMembers()));
-        System.out.println(party.addMember(null));
-        System.out.println(partyAnalyzer.countRoles(party.getMembers()));
+        System.out.println(party.addMember(theSeparateArthur));
         System.out.println(party.addMember(lancelot));
         System.out.println(partyAnalyzer.countRoles(party.getMembers()));
-        System.out.println(party.addMember(theSeparateArthur));
+        System.out.println(partyAnalyzer.hasDamage(partyAnalyzer.countRoles(party.getMembers())));
+        System.out.println(party.addMember(theSeparateLancelot));
+        System.out.println(party.addMember(moon));
+        System.out.println(party.addMember(sun));
+        System.out.println(party.addMember(star));
+        System.out.println(party.addMember(astroid));
         System.out.println(partyAnalyzer.countRoles(party.getMembers()));
-        System.out.println(party.removeMember(theSeparateArthur));
-        System.out.println(party.removeMember(lancelot));
-        System.out.println(party.removeMember(arthur));
-        System.out.println(partyAnalyzer.countRoles(party.getMembers()));
-        System.out.println(partyAnalyzer.countRoles(null));
+        System.out.println(partyAnalyzer.hasComplementaryRole(partyAnalyzer.countRoles(party.getMembers())));
+        System.out.println(partyAnalyzer.countDamageRoles(partyAnalyzer.countRoles(party.getMembers())));
+        System.out.println(partyAnalyzer.countComplementaryRoles(partyAnalyzer.countRoles(party.getMembers())));
+        System.out.println(partyAnalyzer.calculateRoleRating(partyAnalyzer.countRoles(party.getMembers())));
 
+        
     }
 }
